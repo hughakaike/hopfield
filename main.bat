50d1cc9a9f0795d2f86c58b1b57ca72e977e6b03
hughakaike <101465037+hughakaike@users.noreply.github.com>
2025-03-22 22:50:25 +0900

    Add files via upload

--- main.bat
+++ main.bat
@@ -2,6 +2,13 @@ set INPUT_DIR=input
 set OUTPUT_DIR=output
 set SCRIPT_DIR=scr
 
+echo convert image to matrix
 python %SCRIPT_DIR%/convert2matrix.py %INPUT_DIR%
+pause
+
+echo start hopfield main
 start main.exe %INPUT_DIR% %OUTPUT_DIR%
+pause
+
+echo convert matrix to image
 python %SCRIPT_DIR%/convert2image.py %OUTPUT_DIR%
